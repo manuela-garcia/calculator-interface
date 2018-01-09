a967ff18db36c44d3e0cf671f0141ba3498a1efc
Nanette Girzi and Shirley Dennis <nanette@>
2018-01-08 16:42:10 -0800

    add BMI calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
-var add = function(number1, number2) {
-return number1 + number2;
+var divide = function(number1, number2) {
+return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"))
-var number2 = parseInt(prompt("Enter another number"))
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = divide(number1, number2);
 
-alert(add(number1, number2));
+alert(result);
+
+var bmi = function(weight, height) {
+  return Math.round(weight / (height * height));
+};
+
+var weight = parseFloat(prompt("Enter your weight in kilograms:"));
+var height = parseFloat(prompt("Enter your height in meters:"));
+
+alert(bmi(weight, height));
