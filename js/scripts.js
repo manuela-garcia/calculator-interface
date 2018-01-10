bfcf1badc16f86675781d127370abcf2ba96c48c
Manuela Garcia and Nanette Girzi <manuela@>
2018-01-10 13:44:30 -0800

    add user interface logic to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
+
+// Business logic:
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
 var divide = function(number1, number2) {
-return number1 / number2;
+  return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
+// Everything bleow this line is user interface logic:
 
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
 
-var bmi = function(weight, height) {
-  return Math.round(weight / (height * height));
-};
-
-var weight = parseFloat(prompt("Enter your weight in kilograms:"));
-var height = parseFloat(prompt("Enter your height in meters:"));
-
-alert(bmi(weight, height));
-
-var temp = function(celcius) {
-  return celcius * 1.8 + 32;
-};
-
-var celcius = parseFloat(prompt("Enter degrees celcuis:"));
-
-alert(temp(celcius))
+  });
+});
